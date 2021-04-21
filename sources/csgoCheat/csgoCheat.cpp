8d364b4d669e5dc26cd414121126a242ffa407a9
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-04-22 01:09:46 +0200

    improved log output

--- sources/csgoCheat/csgoCheat.cpp
+++ sources/csgoCheat/csgoCheat.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <windows.h>
 
 #include "Memory.h"
 #include "Bhop.h"
@@ -12,10 +13,11 @@ using namespace std;
 
 bool triggerToggle = false; // triggerbot off/on
 bool whToggle = false;
+bool bhobToggle = false;
 
 // initializes the Cheat
 void Init() {
-    cout << "Initiating engine" << endl;
+    cout << "Initiating engine..." << endl;
     mem.Setup();
     mem.GetModules();
     weaponSkins.ChangeSkinLayout();
@@ -25,26 +27,34 @@ void Init() {
 void toggleFeatures() {
     if (GetKeyState('V') < 0 && triggerToggle == false) {
         triggerToggle = true;
-        cout << "Triggerbot turned on." << endl;
+        cout << "Triggerbot on" << endl;
     }
     else if (GetKeyState('V') < 0 && triggerToggle == true) {
         triggerToggle = false;
-        cout << "Triggerbot turned off." << endl;
+        cout << "Triggerbot off" << endl;
     }
     else if (GetKeyState('X') < 0 && whToggle == false) {
         whToggle = true;
-        cout << "Wallhack turned on." << endl;
+        cout << "Wallhack on" << endl;
     }
     else if (GetKeyState('X') < 0 && whToggle == true) {
         whToggle = false;
-        cout << "Wallhack turned off." << endl;
+        cout << "Wallhack off" << endl;
+    }
+    else if (GetKeyState('C') < 0 && bhobToggle == false) {
+        bhobToggle = true;
+        cout << "Bhop on" << endl;
+    }
+    else if (GetKeyState('C') < 0 && bhobToggle == true) {
+        bhobToggle = false;
+        cout << "Bhop off" << endl;
     }
 }
 
 
 void NoDelayFeatures() {
     cout << "--> Triggerbot ready (Toggle it with V)" << endl;
-    cout << "--> Bhop activated" << endl;
+    cout << "--> Bhop ready       (Toggle it with C)" << endl;
     while (true) {
         if (triggerToggle) {
             trigger.Run();
@@ -56,7 +66,7 @@ void NoDelayFeatures() {
 
 void DelayFeatures() {
     Sleep(10);
-    cout << "--> Wallhack ready (Toggle it with X)" << endl;
+    cout << "--> Wallhack ready   (Toggle it with X)" << endl;
     
     while (true) {
         if (whToggle) {
@@ -69,11 +79,13 @@ void DelayFeatures() {
 
 
 void SkinChangerThread() {
-    Sleep(20);
+    Sleep(30);
     cout << "--> Random skinchanger activated" << endl;
     cout << "" << endl;
-    cout << "<------------------------------------>" << endl;
-    cout << "" << endl;
+    cout << "<-------------------------------------->" << endl;
+    cout << "\n\n" << endl;
+    cout << "<---- Toggle Log ---->\n" << endl;
+    cout << "Triggerbot off\nWallhack off\nBhob off" << endl;
 
     while (true) {
         skinChanger.Run();
@@ -86,8 +98,13 @@ int main()
 {
 
     SetConsoleTitle(L"CS:GO Moerper");
-    cout << "         CS:GO Moerper - LOG" << endl;
-    cout << "<------------------------------------>" << endl;
+    HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r);
+    MoveWindow(console, r.left, r.top, 400, 800, TRUE);
+
+    cout << "             CS:GO Moerper" << endl;
+    cout << "<-------------------------------------->" << endl;
     cout << "waiting for CS:GO..." << endl;
 
     Init();
