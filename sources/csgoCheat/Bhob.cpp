d71cfcaec06832a7ebff9a2a2be0b321d32572ef
Matthias Hottgenroth <matthias.hottgenroth@icloud.com>
2021-04-27 16:42:46 +0200

    Bhop - comments

--- sources/csgoCheat/Bhob.cpp
+++ sources/csgoCheat/Bhob.cpp
@@ -7,10 +7,13 @@ Bhop bhop;
 using namespace std;
 
 void Bhop::Run() {
+	// Get flag
 	DWORD localPlayer = mem.ReadMemory<DWORD>(offsets.clientBase + offsets.dwLocalPlayer);
 	int flags = mem.ReadMemory<int>(localPlayer + offsets.m_fFlags);
 	
+	// Checks if player touches the ground and presses Space
 	if ((flags & (1 << 0)) && (GetAsyncKeyState(VK_SPACE) < 0)) {
+		// Jumps
 		mem.WriteMemory<DWORD>(offsets.clientBase + offsets.dwForceJump, 6);
 	}
 }
