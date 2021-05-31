faaef45eedc25622ae1a991f7efefecb8728076c
moerp <matthias@hottgenroth.net>
2021-06-01 01:44:56 +0200

    create object in code

--- sources/GUI/mainwindow.cpp
+++ sources/GUI/mainwindow.cpp
@@ -7,6 +7,44 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle("The Moerper external");
+
+    // Get Objects
+    checkWallhack = ui->checkWallhack;
+    checkRadar = ui->checkRadar;
+    checkAimbot = ui->checkAimbot;
+    checkTrigger = ui->checkTrigger;
+    checkSkinchanger = ui->checkSkinchanger;
+    checkKnifechanger = ui->checkKnifechnager;
+    checkBhop = ui->checkBhop;
+    checkNoflash = ui->checkNoflash;
+
+    radioRed = ui->radioRed;
+    radioGreen = ui->radioGreen;
+    radioBlue = ui->radioBlue;
+
+    pushChangeSkin = ui->pushChangeSkin;
+    pushChangeKeys = ui->pushChangeKeys;
+    pushReset = ui->pushReset;
+    pushQuit = ui->pushQuit;
+
+    // Connections
+    connect(checkWallhack, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkRadar, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkAimbot, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkTrigger, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkSkinchanger, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkKnifechanger, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkBhop, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+    connect(checkNoflash, SIGNAL(stateChanged(int)), this, SLOT(CheckboxChanged()));
+
+    connect(radioRed, SIGNAL(clicked(bool)), this, SLOT(CheckboxChanged()));
+    connect(radioGreen, SIGNAL(clicked(bool)), this, SLOT(CheckboxChanged()));
+    connect(radioBlue, SIGNAL(clicked(bool)), this, SLOT(CheckboxChanged()));
+
+    connect(pushChangeSkin, SIGNAL(clicked()), this, SLOT(ChangeSkinlayout()));
+    connect(pushChangeKeys, SIGNAL(clicked()), this, SLOT(ChangeHotkeys()));
+    connect(pushReset, SIGNAL(clicked()), this, SLOT(Resetproccess()));
+    connect(pushQuit, SIGNAL(clicked()), this, SLOT(Quit()));
 }
 
 MainWindow::~MainWindow()
--- sources/GUI/mainwindow.h
+++ sources/GUI/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+class QPushButton;
+class QCheckBox;
+class QRadioButton;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +19,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void CheckboxChanged();
+    void ChangeHotkeys();
+    void Resetproccess();
+    void Quit();
+    void ChangeSkinlayout();
+
 private:
     Ui::MainWindow *ui;
+
+    QCheckBox *checkWallhack, *checkRadar, *checkAimbot, *checkTrigger,
+              *checkSkinchanger, *checkKnifechanger, *checkBhop, *checkNoflash;
+    QRadioButton *radioRed, *radioGreen, *radioBlue;
+    QPushButton *pushChangeSkin, *pushChangeKeys, *pushReset, *pushQuit;
 };
 #endif // MAINWINDOW_H
