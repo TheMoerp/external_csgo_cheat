1e7af8de5f4bc3c238bf6e45bb73bdf6c8bfa6f3
moerp <matthias@hottgenroth.net>
2021-06-01 15:23:58 +0200

    fetched ui objects

--- sources/GUI/changeskinlayout.cpp
+++ sources/GUI/changeskinlayout.cpp
@@ -5,4 +5,40 @@ ChangeSkinLayout::ChangeSkinLayout(QWidget *parent)
 {
     ui.setupUi(this);
     setWindowTitle("Skinlayout");
+
+    lineAK47 = ui.lineAK47;
+    lineAUG = ui.lineAUG;
+    lineAWP = ui.lineAWP;
+    lineCZ75 = ui.lineCZ75;
+    lineDesartEagle = ui.lineDesartEagle;
+    lineDualBerettas = ui.lineDualBerettas;
+    lineFAMAS = ui.lineFAMAS;
+    lineFiveSeven = ui.lineFiveSeven;
+    lineG35G1 = ui.lineG35G1;
+    lineGalilAR = ui.lineGalilAR;
+    lineGlock18 = ui.lineGlock18;
+    lineKnifeSkin = ui.lineKnifeSkin;
+    lineM249 = ui.lineM249;
+    lineM4A1S = ui.lineM4A1S;
+    lineM4A4 = ui.lineM4A4;
+    lineMAC10 = ui.lineMAC10;
+    lineMAG7 = ui.lineMAG7;
+    lineMP5SD = ui.lineMP5SD;
+    lineMP7 = ui.lineMP7;
+    lineMP9 = ui.lineMP9;
+    lineNegev = ui.lineNegev;
+    lineNova = ui.lineNova;
+    lineP2000 = ui.lineP2000;
+    lineP250 = ui.lineP250;
+    lineP90 = ui.lineP90;
+    linePPBizon = ui.linePPBizon;
+    lineRevolver = ui.lineRevolver;
+    lineSCAR20 = ui.lineSCAR20;
+    lineSG553 = ui.lineSG553;
+    lineSSG08 = ui.lineSSG08;
+    lineSawedOff = ui.lineSawedOff;
+    lineTec9 = ui.lineTec9;
+    lineUMP45 = ui.lineUMP45;
+    lineUSPS = ui.lineUSPS;
+    lineXM1014 = ui.lineXM1014;
 }
